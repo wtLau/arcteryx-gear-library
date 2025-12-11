3fe68b44ef3a134255fb14caac4df79fc575456b
Brian Lau <brian.lau.external@arcteryx.com>
2025-12-11 13:29:50 -0800

    Revert "feat: adding linaria"
    This reverts commit 76fc1e434f213783a6e33254ec54f1a4547495fa.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import withLinaria, { LinariaConfig } from 'next-with-linaria';
 
-const nextConfig: LinariaConfig = {
+const nextConfig: NextConfig = {
   /* config options here */
-  linaria: {
-    fastCheck: false,
-  },
 };
 
-export default withLinaria(nextConfig);;
+export default nextConfig;
